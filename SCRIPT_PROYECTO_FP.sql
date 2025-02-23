1d069d3c96f493146f50670462b14be58b1389ca
pau-05 <paulandre04517@gmail.com>
2025-02-23 21:36:26 +0100

    Add files via upload

---
+++ SCRIPT_PROYECTO_FP.sql
@@ -0,0 +1,62 @@
+CREATE TABLE USUARIO(
+    id_ususario NUMBER(8) PRIMARY KEY,
+    apellido1 VARCHAR2(30) NOT NULL,
+    apellido2 VARCHAR2(30) NOT NULL,
+    email VARCHAR2(20) UNIQUE,
+    nombre VARCHAR2(30) NOT NULL,
+    contrasenia VARCHAR2(15) NOT NULL
+);
+
+CREATE TABLE CATEGORÍA(
+    nombre VARCHAR(30) PRIMARY KEY,
+    descripcion VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE ORGANIZADOR(
+    NIF VARCHAR2(9) PRIMARY KEY,
+    nombre VARCHAR2(20) NOT NULL,
+    dirección VARCHAR2(40)
+);
+
+CREATE TABLE ORGANIZADOR_TELF(
+    NIF VARCHAR2(9),
+    email VARCHAR2(20),
+    PRIMARY KEY (email, NIF),
+    FOREIGN KEY (NIF) REFERENCES ORGANIZADOR(NIF)
+);
+
+CREATE TABLE ORGANIZADOR_EMAIL (
+    NIF VARCHAR2(9),
+    tlfno NUMBER(9),
+    PRIMARY KEY (tlfno, NIF),
+    FOREIGN KEY (NIF) REFERENCES ORGANIZADOR(NIF)
+);
+
+CREATE TABLE EVENTO(
+    id_evento NUMBER(6) PRIMARY KEY,
+    direccion VARCHAR2(40),
+    duracion INTERVAL DAY TO SECOND NOT NULL,
+    fecha_h TIMESTAMP NOT NULL,
+    nombre VARCHAR2(30) NOT NULL,
+    descripción VARCHAR2(150) NOT NULL
+);
+
+CREATE TABLE USUARIO_EVENTO_INSCRIPCION(
+    id_usuario NUMBER(8),
+    id_evento NUMBER(6),
+    fecha_h TIMESTAMP NOT NULL,
+    PRIMARY KEY (id_usuario, id_evento),
+    FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_ususario),
+    FOREIGN KEY (id_evento) REFERENCES EVENTO(id_evento)
+);
+
+CREATE TABLE USUSARIO_EVENTO_ASISTENCIA(
+    id_usuario NUMBER(8),
+    id_evento NUMBER(6),
+    asistencia NUMBER(1), --0 para true y 1 para false
+    PRIMARY KEY (id_usuario, id_evento),
+    FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_ususario),
+    FOREIGN KEY (id_evento) REFERENCES EVENTO(id_evento),
+    CONSTRAINT ck_booleano CHECK (asistencia IN (0, 1))
+    --la constraint revisará que solo se ponga 0 o 1 en asistencia
+);
