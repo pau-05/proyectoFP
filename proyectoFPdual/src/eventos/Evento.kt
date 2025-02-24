1232aaef63ff42cf4a3b32382c1ac38a28501311
pau-05 <paulandre04517@gmail.com>
2025-02-24 19:55:29 +0100

    cambios y métodos añadidos

--- proyectoFPdual/src/eventos/Evento.kt
+++ proyectoFPdual/src/eventos/Evento.kt
@@ -1,13 +1,56 @@
 package proyectoFPdual.eventos
 
-class Evento(private var nombre: String, private var ubicacion: String, private var fecha: String) {
-    //private var EventosDisp = ArrayList<Evento>
-    private var categoria = arrayListOf("taller", "actividad", "conferencia")
-/*en este metodo una vez iniciada la sesión se debe poder añadir eventos por tanto las clases deben colaborar,
-* tmb se debe poder añadir al usuario algún evento así que se debe de crear una lista de eventos por cada usuario
-* ahora, solo los admin pueden editar los eventos y añadirlos, por lo tanto si no cumple la clave "admin" (o pedir un código adicional)
-* no puede editar ni crear, solo añadir eventos  a su lista*/
-    fun crearEvento(nombre: String, ubicacion: String, fecha: String){
+class Evento(private var nombreEvento: String, private var ubicacion: String, private var fecha: String,
+             private var categoria: String) {
 
+    companion object{
+        private var EventosDisp = ArrayList<Evento>()
+        private var EventosActividad = ArrayList<Evento>()
+        private var EventosConferencia = ArrayList<Evento>()
+        private var EventosTalleres = ArrayList<Evento>()
+    /*en este metodo una vez iniciada la sesión se debe poder añadir eventos por tanto las clases deben colaborar,
+    * tmb se debe poder añadir al usuario algún evento así que se debe de crear una lista de eventos por cada usuario
+    * ahora, solo los admin pueden editar los eventos y añadirlos, por lo tanto si no cumple la clave "admin" (o pedir un código adicional)
+    * no puede editar ni crear, solo añadir eventos  a su lista
+    *
+    * 24/2: lo que necesito hacer es usar la dependencia para que una vez inicie sesión con el admin se lo traiga aquí y pueda modificar
+    * los datos en los eventos (y crearlos), en lo sotros casos solo podrá unirse a los eventos.
+    *
+    * Añadir: que ignore mayus y minus, array con eventos creados por el admin, mirar como hacer perfiles de admins, comprobación
+    * de fecha, listas de eventos por cada admin (se tiene que crear automaticamente una lista vacía cuando se crea un admin) y
+    * crear una lista para cada categoria d esta manera se imprimirán 3 al solicitar ver los eventos*/
+
+        init {
+            //Inicializo por lo menos un evento que puedan agregar los usuarios
+            EventosDisp.add(Evento("Recolecta de basura", "Parque del sol, Puertollano", "20/04/2025", "Actividad"))
+        }
+
+        fun crearEvento(nombreEvento: String, ubicacion: String, fecha: String, categoria: String){
+            val nuevoEvento = Evento(nombreEvento, ubicacion, fecha, categoria)
+            if (!comprobarEvento(nombreEvento, ubicacion, fecha, categoria)){
+                EventosDisp.add(nuevoEvento)
+            }
+        }
+
+        //Método que le permite a los admins modificar los eventos
+        fun modificarEvento(){
+
+        }
+
+        //Método que le permite a los usuarios apuntarse a eventos
+        fun aniadirEvento(){
+
+        }
+
+        //Método que comprueba que el evento que se quiera crear o borrar exista
+        fun comprobarEvento(nombreEvento: String, ubicacion: String, fecha: String, categoria: String): Boolean{
+            for (evento in EventosDisp) {
+                if (evento.nombreEvento == nombreEvento && evento.ubicacion == ubicacion && evento.fecha == fecha
+                    && evento.categoria== categoria){
+                    return true
+                }
+            }
+            return false
+        }
     }
 }
--- proyectoFPdual/src/main.kt
+++ proyectoFPdual/src/main.kt
@@ -1,29 +1,70 @@
 package proyectoFPdual
 
-import proyectoFPdual.eventos.Usuario
+import proyectoFPdual.eventos.*
+import proyectoFPdual.eventos.Usuario.Companion.getAdmin
 
 fun main() {
     // Inicializo las variables que va a rellenar el usuario durante el uso de los menús:
     var usr = ""
     var contra = ""
-    val nomEvento = ""
-    var inicioUsr = false
+    var correo = ""
+    var nomEvento = ""
+    var ubiEvento = ""
+    var fechaEvento = ""
+    var cat = ""
+
 
     println("Bienvenido al portal de sostenibilidad! Para continuar seleccione alguna de las opciones:\n" +
                 "1. Iniciar sesión\n" +
                 "2. Crear cuenta"
     )
     val primerMenu = readln().toInt()
-    when (primerMenu) {
+    when (primerMenu){
         1 -> {
             println("Escribe el nombre del usuario: ")
             usr = readln()
             println("Escribe la contraseña: ")
             contra = readln()
-            val comprobarUsr = Usuario.comprobarUsuario(usr, contra)
+            println("Escribe tu correo: ")
+            correo = readln()
+            val comprobarUsr = Usuario.comprobarUsuario(usr, contra, correo)
             if (comprobarUsr) {
-                println("Inicio de sesión exitoso.")
-                inicioUsr = true
+                println("Inicio de sesión exitoso. Seleccione una la acción que desea realizar: \n" +
+                        "1. Apuntarse a evento\n" +
+                        "2. Crear evento\n" +
+                        "3. Modificar evento\n"
+                )
+                val segMenu= readln().toInt()
+                when(segMenu){
+                    1 -> {
+                        println("Dime a que evento quieres apuntarte: ")
+                        val evento = readln()
+                    }
+                    2 -> {
+                        var permiso = getAdmin()
+                        if (permiso){
+                            println("Lo sentimos, pero no tienes los permisos necesarios para hacer cambios. " +
+                                    "Inicie sesión como admin")
+                        }else{
+                            println("Bienvenido admin, escriba el nombre del evento que quieras crear: ")
+                            nomEvento = readln()
+                            println("Dime la fecha: ")
+                            fechaEvento = readln()
+                            println("Dime la ubicación del evento")
+                            ubiEvento= readln()
+                            println("Dime la categoría a la que pertenece: ")
+                            cat = readln()
+                            val comporbarEvento = Evento.comprobarEvento(nomEvento, ubiEvento, fechaEvento, cat)
+                            if (comporbarEvento){
+                                println("Evento ya existente, pruebe a crear otro evento.")
+                            }else{
+                                val nuevoEvento = Evento.crearEvento(nomEvento, ubiEvento, fechaEvento, cat)
+                                println("Evento creado con éxito.")
+                            }
+                        }
+                    }
+                    3 -> {}
+                }
             } else {
                 println("Usuario o contraseña incorrectos.")
             }
@@ -34,12 +75,13 @@ fun main() {
             usr = readln()
             println("Escribe la contraseña: ")
             contra = readln()
-            val comprobarUsr = Usuario.comprobarUsuario(usr, contra)
+            println("Escribe tu correo: ")
+            correo = readln()
+            val comprobarUsr = Usuario.comprobarUsuario(usr, contra, correo)
             if (comprobarUsr) {
-                println("Usuario ya existente, pruebe a iniciar sesión o cambie la contraseña si desea.")
+                println("Usuario ya existente, pruebe a iniciar sesión.")
             } else {
-                val nuevoUsuario = Usuario.crearUsuario(usr, contra)
-                Usuario.agregarUsuario(nuevoUsuario)
+                val nuevoUsuario = Usuario.crearUsuario(usr, contra, correo)
                 println("Usuario creado con éxito.")
             }
         }
